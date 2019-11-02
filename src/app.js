2b747f4e3fb2d97807b5094a2e46a96cccbe5b7c
manaraph <manaraph225@gmail.com>
2019-11-02 14:59:57 +0100

    Create and link to index.ejs

--- src/app.js
+++ src/app.js
@@ -6,4 +6,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Index'})
+});
+
+app.listen(3000, () => console.log('Project Started on port 3000'));
