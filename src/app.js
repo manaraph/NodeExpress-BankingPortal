b26534016f5ae846c488e4b5781e80c5af61a4a3
manaraph <manaraph225@gmail.com>
2019-11-03 20:42:57 +0100

    Handling form data, transfer and payment

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
 const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8'});
 const userData  = fs.readFileSync('src/json/users.json', { encoding: 'utf8'});
@@ -16,14 +17,13 @@ const users  = JSON.parse(userData);
 // console.log(accounts);
 // console.log(users);
 
-
+// Get routes
 app.get('/', (req, res) => {
   res.render('index', { 
     title: 'Account Summary',
     accounts,
   })
 });
-
 
 app.get('/savings', (req, res) => {
   res.render('account', { account: accounts.savings })
@@ -37,8 +37,37 @@ app.get('/credit', (req, res) => {
   res.render('account', { account: accounts.savings })
 });
 
+app.get('/transfer', (req, res) => {
+  res.render('transfer', { account: accounts.savings })
+});
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8');
+
+  res.render('transfer', { message: "Transfer Completed" })
+});
+
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit })
+});
+
+app.post('/payment', (req, res) => {
+  accounts.credit.balance = accounts.credit.balance - req.body.amount;
+  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
+
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8');
+
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+});
+
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] })
 });
+
 
 app.listen(3000, () => console.log('Project Started on port 3000'));
